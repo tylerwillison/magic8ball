f165deff2533c81c88893e1cacab6e55fcc28832
Tyler <tyler.willison.2022@student.lhschools.org>
2019-09-30 09:54:54 -0400

    initialized all the variables ill need.

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,46 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner keyboard;
+	keyboard = new Scanner(System.in);
+
+	    int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+        int fourthNumber;
+        int fifthNumber;
+        int sixthNumber;
+        int seventhNumber;
+        int eighthNumber;
+        int ninthNumber;
+        int tenthNumbers;
+        Double number;
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
